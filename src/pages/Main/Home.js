d01aaaeb4e9ef3c6db9a7d08c2dbf31ee3b4de08
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-12-04 19:55:32 +0600

    added reducer logic for toggle brand action

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,39 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import { toggleBrand } from "../../redux/actions/filterActions";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-redux-thunk/main/public/products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+    useEffect(() => {
+        fetch(
+            "https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-redux-thunk/main/public/products.json"
+        )
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
 
-  const activeClass = "text-white  bg-indigo-500 border-white";
+    const activeClass = "text-white  bg-indigo-500 border-white";
 
-  return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
-        <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
-        >
-          In Stock
-        </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
-          AMD
-        </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
-          Intel
-        </button>
-      </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="max-w-7xl gap-14 mx-auto my-10">
+            <div className="mb-10 flex justify-end gap-5">
+                <button
+                    className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+                >
+                    In Stock
+                </button>
+                <button
+                    onClick={() => dispatch(toggleBrand("amd"))}
+                    className={`border px-3 py-2 rounded-full font-semibold`}
+                >
+                    AMD
+                </button>
+                <button
+                    onClick={() => dispatch(toggleBrand("intel"))}
+                    className={`border px-3 py-2 rounded-full font-semibold`}
+                >
+                    Intel
+                </button>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
+                {products.map((product) => (
+                    <ProductCard key={product.model} product={product} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
