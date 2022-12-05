e8276dcf7715b9ec5baa81245b971ac2207cb740
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-12-05 23:11:42 +0600

    added custom middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
 
-const store = createStore(rootReducer, composeWithDevTools());
+// composeWithDevTools()
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(cartCounter))
+);
 
 export default store;
