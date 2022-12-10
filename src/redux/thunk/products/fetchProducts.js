155ec7974c0c942aff7625fd70cc778c59d6570b
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-12-10 13:22:42 +0600

    fetched data from database

--- src/redux/thunk/products/fetchProducts.js
+++ src/redux/thunk/products/fetchProducts.js
@@ -2,12 +2,10 @@ import { loadProduct } from "../../actions/productAction";
 
 export const loadProductData = () => {
     return async (dispatch, getState) => {
-        const resp = await fetch(
-            "https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-redux-thunk/main/public/products.json"
-        );
+        const resp = await fetch("http://localhost:5000/products");
         const data = await resp.json();
-        if (data.length) {
-            dispatch(loadProduct(data));
+        if (data?.data?.length) {
+            dispatch(loadProduct(data.data));
         }
     };
 };
